40254bcce5795f7ee687930b222ea645351fd810
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-11 13:46:46 -0400

    feat(Domain): criação das interfaces IRepository.

---
+++ Livraria - Back-end/Livraria-API/Domain.Core/Interfaces/Repositorys/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Core.Interfaces.Repositorys
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        void Update(TEntity obj);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+}
