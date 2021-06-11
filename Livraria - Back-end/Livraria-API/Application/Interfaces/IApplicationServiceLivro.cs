cf5abb279d4d123b674ec8e6371e071d8b2e8be7
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-11 17:15:58 -0400

    feat(Application): criação das interfaces IApplicationServices.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/IApplicationServiceLivro.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationServiceLivro
+    {
+        void Add(LivroDto livroDto);
+        void Update(LivroDto livroDto);
+        IEnumerable<LivroDto> GetAll();
+        LivroDto GetById(int id);
+    }
+}
---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/IApplicationServiceUsuario.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationServiceUsuario
+    {
+        void Add(UsuarioDto usuarioDto);
+        void Update(UsuarioDto usuarioDto);
+        IEnumerable<UsuarioDto> GetAll();
+        UsuarioDto GetById(int id);
+    }
+}
