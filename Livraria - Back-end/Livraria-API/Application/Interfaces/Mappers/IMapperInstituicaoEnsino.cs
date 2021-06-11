ba72f7d9bbdfedcc58189016d5305eb60789923b
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-11 16:08:47 -0400

    feat(Application): criação das interfaces IMappers.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/Mappers/IMapperInstituicaoEnsino.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using Domain.Entitys;
+using System.Collections.Generic;
+
+namespace Application.Interfaces.Mappers
+{
+    public interface IMapperInstituicaoEnsino
+    {
+        InstituicaoEnsino MapperDtoToEntity(InstituicaoEnsinoDto instituicaoEnsinoDto);
+        IEnumerable<InstituicaoEnsinoDto> MapperListInstituicaoEnsinosDto(IEnumerable<InstituicaoEnsino> instituicaoEnsino);
+        InstituicaoEnsinoDto MapperEntityToDto(InstituicaoEnsino instituicaoEnsino);
+    }
+}
---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/Mappers/IMapperLivro.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos;
+using Domain.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Mappers
+{
+    public interface IMapperLivro
+    {
+        Livro MapperDtoToEntity(LivroDto livroDto);
+        IEnumerable<LivroDto> MapperListLivrosDto(IEnumerable<Livro> livro);
+        LivroDto MapperEntityToDto(Livro livro);
+    }
+}
