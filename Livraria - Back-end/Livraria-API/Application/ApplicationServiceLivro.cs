821ce13502cbea0f95d30d4011cbec7988cba053
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-12 16:44:13 -0400

    feat(Application): criação da classe ApplicationServiceLivro.

---
+++ Livraria - Back-end/Livraria-API/Application/ApplicationServiceLivro.cs
@@ -0,0 +1,45 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Application.Interfaces.Mappers;
+using Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+    public class ApplicationServiceLivro : IApplicationServiceLivro
+    {
+        private readonly IServiceLivro _serviceLivro;
+        private readonly IMapperLivro _mapperLivro;
+
+        public ApplicationServiceLivro(IServiceLivro serviceLivro, IMapperLivro mapperLivro)
+        {
+            _serviceLivro = serviceLivro;
+            _mapperLivro = mapperLivro;
+        }
+
+        public void Add(LivroDto livroDto)
+        {
+            var livro = _mapperLivro.MapperDtoToEntity(livroDto);
+            _serviceLivro.Add(livro);
+        }
+
+        public IEnumerable<LivroDto> GetAll()
+        {
+            var livros = _serviceLivro.GetAll();
+            return _mapperLivro.MapperListLivrosDto(livros);
+        }
+
+        public LivroDto GetById(int id)
+        {
+            var livro = _serviceLivro.GetById(id);
+            return _mapperLivro.MapperEntityToDto(livro);
+        }
+
+        public void Update(LivroDto livroDto)
+        {
+            var livro = _mapperLivro.MapperDtoToEntity(livroDto);
+            _serviceLivro.Update(livro);
+        }
+    }
+}
