1d440538a535477f00e0103247d4f0f6f7e844b9
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-12 16:08:03 -0400

    feat(Application): criação da classe ApplicationServiceUsuario.

---
+++ Livraria - Back-end/Livraria-API/Application/ApplicationServiceUsuario.cs
@@ -0,0 +1,45 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Application.Interfaces.Mappers;
+using Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Application
+{
+    public class ApplicationServiceUsuario : IApplicationServiceUsuario
+    {
+        private readonly IServiceUsuario _serviceUsuario;
+        private readonly IMapperUsuario _mapperUsuario;
+
+        public ApplicationServiceUsuario(IServiceUsuario serviceUsuario, IMapperUsuario mapperUsuario)
+        {
+            _serviceUsuario = serviceUsuario;
+            _mapperUsuario = mapperUsuario;
+        }
+
+        public void Add(UsuarioDto usuarioDto)
+        {
+            var usuario = _mapperUsuario.MapperDtoToEntity(usuarioDto);
+            _serviceUsuario.Add(usuario);
+        }
+
+        public IEnumerable<UsuarioDto> GetAll()
+        {
+            var usuarios = _serviceUsuario.GetAll();
+            return _mapperUsuario.MapperListUsuariosDto(usuarios);
+        }
+
+        public UsuarioDto GetById(int id)
+        {
+            var usuario = _serviceUsuario.GetById(id);
+            return _mapperUsuario.MapperEntityToDto(usuario);
+        }
+
+        public void Update(UsuarioDto usuarioDto)
+        {
+            var usuario = _mapperUsuario.MapperDtoToEntity(usuarioDto);
+            _serviceUsuario.Update(usuario);
+        }
+    }
+}
