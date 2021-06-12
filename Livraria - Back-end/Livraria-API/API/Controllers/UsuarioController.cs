b39dab65ee7bcfbbfbb6c87bf5218993d02c807d
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-12 19:07:30 -0400

    feat(API): criação do UsuarioController.

---
+++ Livraria - Back-end/Livraria-API/API/Controllers/UsuarioController.cs
@@ -0,0 +1,70 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuarioController : Controller
+    {
+        private readonly IApplicationServiceUsuario _applicationServiceUsuario;
+
+        public UsuarioController(IApplicationServiceUsuario applicationServiceUsuario)
+        {
+            _applicationServiceUsuario = applicationServiceUsuario;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicationServiceUsuario.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_applicationServiceUsuario.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] UsuarioDto usuarioDto)
+        {
+            try
+            {
+                if (usuarioDto == null)
+                {
+                    return NotFound();
+                }
+
+                _applicationServiceUsuario.Add(usuarioDto);
+                return Ok("Usuário Cadastrado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Update([FromBody] UsuarioDto usuarioDto)
+        {
+            try
+            {
+                if (usuarioDto == null)
+                {
+                    return NotFound();
+                }
+
+                _applicationServiceUsuario.Update(usuarioDto);
+                return Ok("Usuario Alterado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
