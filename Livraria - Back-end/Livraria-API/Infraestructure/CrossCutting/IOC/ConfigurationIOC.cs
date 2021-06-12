c7515d97e63e08ac6bf2adb596f11956b04e4093
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-12 17:34:27 -0400

    feat(Insfraestructure): Implementação da configuração de IOC utilizando a biblioteca AutoFac.

---
+++ Livraria - Back-end/Livraria-API/Infraestructure/CrossCutting/IOC/ConfigurationIOC.cs
@@ -0,0 +1,38 @@
+﻿using Application;
+using Application.Interfaces;
+using Application.Interfaces.Mappers;
+using Application.Mappers;
+using Autofac;
+using Domain.Core.Interfaces.Repositorys;
+using Domain.Core.Interfaces.Services;
+using Domain.Services;
+using Infraestructure.Data.Repositorys;
+
+namespace Infraestructure.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+
+            builder.RegisterType<ApplicationServiceUsuario>().As<IApplicationServiceUsuario>();
+            builder.RegisterType<ApplicationServiceInstituicaoEnsino>().As<IApplicationServiceInstituicaoEnsino>();
+            builder.RegisterType<ApplicationServiceLivro>().As<IApplicationServiceLivro>();
+
+            builder.RegisterType<ServiceUsuario>().As<IServiceUsuario>();
+            builder.RegisterType<ServiceInstituicaoEnsino>().As<IServiceInstituicaoEnsino>();
+            builder.RegisterType<ServiceLivro>().As<IServiceLivro>();
+
+            builder.RegisterType<MapperEndereco>().As<IMapperEndereco>();
+            builder.RegisterType<MapperUsuario>().As<IMapperUsuario>();
+            builder.RegisterType<MapperInstituicaoEnsino>().As<IMapperInstituicaoEnsino>();
+            builder.RegisterType<MapperLivro>().As<IMapperLivro>();
+
+            builder.RegisterType<RepositoryUsuario>().As<IRepositoryUsuario>();
+            builder.RegisterType<RepositoryInstituicaoEnsino>().As<IRepositoryInstituicaoEnsino>();
+            builder.RegisterType<RepositoryLivro>().As<IRepositoryLivro>();
+            #endregion
+        }
+    }
+}
