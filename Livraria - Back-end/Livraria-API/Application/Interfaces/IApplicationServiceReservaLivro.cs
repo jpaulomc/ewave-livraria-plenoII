d864192590bfa27b75b3ee7b37119aae011d0d7d
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 18:30:22 -0400

    feat(Application): Correção do nome da interface IApplicationServiceReservaLivro.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/IApplicationServiceReservaLivro.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationServiceReservaLivro
+    {
+        void Add(ReservaLivroDto reservaLivroDto);
+        void Update(ReservaLivroDto reservaLivroDto);
+        IEnumerable<ReservaLivroDto> GetAll();
+        ReservaLivroDto GetById(int id);
+    }
+}
