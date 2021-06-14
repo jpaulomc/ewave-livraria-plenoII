63abc74d0adc14f81acb2225070bb629dcffaf5b
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 13:37:55 -0400

    feat(Entity): Implementação da Classe ReservaLivro.

---
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/ReservaLivro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entitys
+{
+    public class ReservaLivro : Base
+    {
+        public int LivroID { get; set; }
+        public int UsuarioID { get; set; }
+        public bool Ativa { get; set; }
+    }
+}
