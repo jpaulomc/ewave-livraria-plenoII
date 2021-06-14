95ec370c817d4d2483dbafdeee58638cd072495d
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 17:57:07 -0400

    feat(Application): Implementação da Classe UsuarioBloqueadoDto.

---
+++ Livraria - Back-end/Livraria-API/Application/Dtos/UsuarioBloqueadoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Application.Dtos
+{
+    public class UsuarioBloqueadoDto
+    {
+        public int Id { get; set; }
+        public DateTime DataBloqueio { get; set; }
+        public DateTime DataLiberacao { get; set; }
+        public bool StatusBloqueio { get; set; }
+        public UsuarioDto UsuarioDto { get; set; }
+        public EmprestimoLivroDto EmprestimoLivroDto { get; set; }
+    }
+}
