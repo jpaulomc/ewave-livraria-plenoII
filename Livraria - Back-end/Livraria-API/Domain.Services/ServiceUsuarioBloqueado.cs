1a7da0a04a8543f0d0ff53f0314380cf27a88cd0
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 17:14:40 -0400

    feat(Domain): Implementação da Classe ServiceUsuarioBloqueado.

---
+++ Livraria - Back-end/Livraria-API/Domain.Services/ServiceUsuarioBloqueado.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Core.Interfaces.Services;
+using Domain.Entitys;
+
+namespace Domain.Services
+{
+    public class ServiceUsuarioBloqueado : ServiceBase<UsuarioBloqueado>, IServiceUsuarioBloqueado
+    {
+        private readonly IRepositoryUsuarioBloqueado _repositoryUsuarioBloqueado;
+
+        public ServiceUsuarioBloqueado(IRepositoryUsuarioBloqueado repositoryUsuarioBloqueado) : base(repositoryUsuarioBloqueado)
+        {
+            _repositoryUsuarioBloqueado = repositoryUsuarioBloqueado;
+        }
+    }
+}
