d272beac28b25d9ef84ec3f94430105a6367975e
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 16:36:52 -0400

    feat(Domain): Implementação da Classe ServiceEmprestimoLivro.

---
+++ Livraria - Back-end/Livraria-API/Domain.Services/ServiceEmprestimoLivro.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Core.Interfaces.Services;
+using Domain.Entitys;
+
+namespace Domain.Services
+{
+    public class ServiceEmprestimoLivro : ServiceBase<EmprestimoLivro>, IServiceEmprestimoLivro
+    {
+        private readonly IRepositoryEmprestimoLivro _repositoryEmprestimoLivro;
+
+        public ServiceEmprestimoLivro(IRepositoryEmprestimoLivro repositoryEmprestimoLivro) : base(repositoryEmprestimoLivro)
+        {
+            _repositoryEmprestimoLivro = repositoryEmprestimoLivro;
+        }
+    }
+}
