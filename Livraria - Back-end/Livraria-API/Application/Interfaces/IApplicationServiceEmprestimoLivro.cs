4cf6684b3da0830e0cec0e4f95ea8287b7f79bb1
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 18:21:21 -0400

    feat(Application): Implementação da interface IApplicationServiceEmprestimoLivro.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/IApplicationServiceEmprestimoLivro.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationServiceEmprestimoLivro
+    {
+        void Add(EmprestimoLivroDto emprestimoLivroDto);
+        void Update(EmprestimoLivroDto emprestimoLivroDto);
+        IEnumerable<EmprestimoLivroDto> GetAll();
+        EmprestimoLivroDto GetById(int id);
+    }
+}
