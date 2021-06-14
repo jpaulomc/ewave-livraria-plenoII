4afc7094e58d9204fef9249d938832b7664a0f4c
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 13:46:29 -0400

    feat(Entity): Implementação da Classe UsuarioBloqueado.

--- Livraria - Back-end/Livraria-API/Domain/Entitys/ReservaLivro.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/ReservaLivro.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain.Entitys
+﻿namespace Domain.Entitys
 {
     public class ReservaLivro : Base
     {
---
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/UsuarioBloqueado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Entitys
+{
+    public class UsuarioBloqueado : Base
+    {
+        public int UsuarioID { get; set; }
+        public int EmprestimoLivroID { get; set; }
+        public DateTime DataBloqueio { get; set; }
+        public DateTime DataLiberacao { get; set; }
+        public bool StatusBloqueio { get; set; }
+
+        public Usuario Usuario { get; set; }
+        public EmprestimoLivro EmprestimoLivro { get; set; }
+    }
+}
