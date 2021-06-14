781ee4f30495570e04b1d10338f36215233e1064
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 16:34:12 -0400

    feat(Domain): Implementação da Classe ServiceAutor.

---
+++ Livraria - Back-end/Livraria-API/Domain.Services/ServiceAutor.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Core.Interfaces.Services;
+using Domain.Entitys;
+
+namespace Domain.Services
+{
+    public class ServiceAutor : ServiceBase<Autor>, IServiceAutor
+    {
+        private readonly IRepositoryAutor _repositoryAutor;
+
+        public ServiceAutor(IRepositoryAutor repositoryAutor) : base(repositoryAutor)
+        {
+            _repositoryAutor = repositoryAutor;
+        }
+    }
+}
