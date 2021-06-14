d99828013da1da0bb450eec0c464b922c2b5e008
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 13:32:31 -0400

    feat(Entity): Implementação da Classe EmprestimoLivro e adição do StatusLivroEnum a classe Livro.

--- Livraria - Back-end/Livraria-API/Domain/Entitys/Livro.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/Livro.cs
@@ -1,5 +1,12 @@
 ﻿namespace Domain.Entitys
 {
+    public enum StatusLivroEnum
+    {
+        Disponível = 1,
+        Emprestado = 2,
+        Reservado = 3
+    }
+
     public class Livro : Base
     {
         public string Titulo { get; set; }
@@ -8,6 +15,7 @@
         public string Sinopse { get; set; }
         public string Capa { get; set; }
         public bool Ativo { get; set; }
+        public StatusLivroEnum StatusLivro { get; set; }
 
         public Genero Genero { get; set; }
         public Autor Autor {get;set;}
