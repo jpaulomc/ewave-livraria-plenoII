ba3be9ced4e5aace54f6b8e95787f56290a022bf
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 14:33:36 -0400

    feat(SqlContext): Implementação das novas propriendades na classe SqlContext.

--- Livraria - Back-end/Livraria-API/Infraestructure/Data/SqlContext.cs
+++ Livraria - Back-end/Livraria-API/Infraestructure/Data/SqlContext.cs
@@ -8,9 +8,16 @@ namespace Infraestructure.Data
         public SqlContext() { }
         public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
 
+        public DbSet<Endereco> Endereco { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
-        public DbSet<InstituicaoEnsino> InstituicaoEnsino { get; set; }
         public DbSet<Livro> Livro { get; set; }
+        public DbSet<InstituicaoEnsino> InstituicaoEnsino { get; set; }
+        public DbSet<Autor> Autor { get; set; }
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<EmprestimoLivro> EmprestimoLivro { get; set; }
+        public DbSet<ReservaLivro> ReservaLivro { get; set; }
+        public DbSet<UsuarioBloqueado> UsuarioBloqueado { get; set; }
+
 
     }
 }
