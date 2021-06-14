62228a825595f2e12f9b2b2f3bbc159da29625d0
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 17:30:00 -0400

    feat(Infraestructure): Implementação da Classe RepositoryGenero.

---
+++ Livraria - Back-end/Livraria-API/Infraestructure/Data/Repositorys/RepositoryGenero.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Entitys;
+
+namespace Infraestructure.Data.Repositorys
+{
+    public class RepositoryGenero : RepositoryBase<Genero>, IRepositoryGenero
+    {
+        private readonly SqlContext _sqlContext;
+
+        public RepositoryGenero(SqlContext sqlContext)
+            : base(sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+    }
+}
