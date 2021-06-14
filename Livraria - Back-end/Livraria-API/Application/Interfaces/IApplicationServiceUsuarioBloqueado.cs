3b76317359b1b8c170ebb907122ad67257153d8e
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 18:32:47 -0400

    feat(Application): Correção do nome da interface IApplicationServiceUsuarioBloqueado.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/IApplicationServiceUsuarioBloqueado.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationServiceUsuarioBloqueado
+    {
+        void Add(UsuarioBloqueadoDto usuarioBloqueadoDto);
+        void Update(UsuarioBloqueadoDto usuarioBloqueadoDto);
+        IEnumerable<UsuarioBloqueadoDto> GetAll();
+        UsuarioBloqueadoDto GetById(int id);
+    }
+}
