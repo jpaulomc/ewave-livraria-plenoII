f9143c71e6d0a9ebb5c0c8201887124bb28e59f6
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 14:02:13 -0400

    feat(Entity): Refatoração da Classe ReservaLivro.

--- Livraria - Back-end/Livraria-API/Domain/Entitys/ReservaLivro.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/ReservaLivro.cs
@@ -5,5 +5,9 @@
         public int LivroID { get; set; }
         public int UsuarioID { get; set; }
         public bool Ativa { get; set; }
+
+        public Livro Livro { get; set; }
+        public Usuario Usuario { get; set; }
+
     }
 }
