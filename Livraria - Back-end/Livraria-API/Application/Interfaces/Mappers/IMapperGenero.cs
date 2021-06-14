91f85c2400578f793d956dbac96232e4428ea219
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 14:54:55 -0400

    feat(Application): Implementação da Interface IMapperGenero e das classes MapperGenero e GeneroDto.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/Mappers/IMapperGenero.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using Domain.Entitys;
+using System.Collections.Generic;
+
+namespace Application.Interfaces.Mappers
+{
+    public interface IMapperGenero
+    {
+        Genero MapperDtoToEntity(GeneroDto generoDto);
+        IEnumerable<GeneroDto> MapperListGenerosDto(IEnumerable<Genero> genero);
+        GeneroDto MapperEntityToDto(Genero genero);
+    }
+}
