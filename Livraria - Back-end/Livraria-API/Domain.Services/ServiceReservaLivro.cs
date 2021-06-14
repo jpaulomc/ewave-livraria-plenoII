ea6e17267269f7c6b2eda913be35ee2b8a47f26e
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 16:49:04 -0400

    feat(Domain): Implementação da Classe ServiceReservaLivro.

---
+++ Livraria - Back-end/Livraria-API/Domain.Services/ServiceReservaLivro.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Core.Interfaces.Services;
+using Domain.Entitys;
+
+namespace Domain.Services
+{
+    public class ServiceReservaLivro : ServiceBase<ReservaLivro>, IServiceReservaLivro
+    {
+        private readonly IRepositoryReservaLivro _repositoryReservaLivro;
+
+        public ServiceReservaLivro(IRepositoryReservaLivro repositoryReservaLivro) : base(repositoryReservaLivro)
+        {
+            _repositoryReservaLivro = repositoryReservaLivro;
+        }
+    }
+}
