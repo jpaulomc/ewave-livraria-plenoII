fcd3d2dac76c5a5c683c46a465404bf571da080e
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 10:26:38 -0400

    feat(Entity): Implementação das Classes Genero e Autor e Refatoração das classe Livro.

---
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/Genero.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entitys
+{
+    public class Genero : Base
+    {
+        public string Descricao { get; set; }
+    }
+}
--- Livraria - Back-end/Livraria-API/Domain/Entitys/Livro.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/Livro.cs
@@ -3,10 +3,14 @@
     public class Livro : Base
     {
         public string Titulo { get; set; }
-        public string Genero { get; set; }
-        public string Autor { get; set; }
+        public string GeneroID { get; set; }
+        public string AutorID { get; set; }
         public string Sinopse { get; set; }
         public string Capa { get; set; }
         public bool Ativo { get; set; }
+
+        public Genero Genero { get; set; }
+        public Autor Autor {get;set;}
+
     }
 }
