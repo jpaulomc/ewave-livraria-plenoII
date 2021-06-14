f61079dde73858a7c8514f6e77f44ed8c2801c24
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 18:05:26 -0400

    feat(Application): Implementação da interface IMapperEmprestimoLivro.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/Mappers/IMapperEmprestimoLivro.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using Domain.Entitys;
+using System.Collections.Generic;
+
+namespace Application.Interfaces.Mappers
+{
+    public interface IMapperEmprestimoLivro
+    {
+        EmprestimoLivro MapperDtoToEntity(EmprestimoLivroDto emprestimoLivroDto);
+        IEnumerable<EmprestimoLivroDto> MapperListEmprestimoLivrosDto(IEnumerable<EmprestimoLivro> emprestimoLivros);
+        EmprestimoLivroDto MapperEntityToDto(EmprestimoLivro emprestimoLivro);
+    }
+}
