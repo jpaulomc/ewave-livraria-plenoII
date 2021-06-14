a33f955967e37b90b183efebfc030972f5502872
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 16:45:04 -0400

    feat(Domain): Implementação da Classe ServiceGenero.

---
+++ Livraria - Back-end/Livraria-API/Domain.Services/ServiceGenero.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Core.Interfaces.Services;
+using Domain.Entitys;
+
+namespace Domain.Services
+{
+    public class ServiceGenero : ServiceBase<Genero>, IServiceGenero
+    {
+        private readonly IRepositoryGenero _repositoryGenero;
+
+        public ServiceGenero(IRepositoryGenero repositoryGenero) : base(repositoryGenero)
+        {
+            _repositoryGenero = repositoryGenero;
+        }
+    }
+}
