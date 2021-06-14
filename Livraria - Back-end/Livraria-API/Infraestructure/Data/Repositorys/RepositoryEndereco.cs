697f10e93aeeb9604c7af0867bd79a8ec275df1c
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 17:28:11 -0400

    feat(Infraestructure): Implementação da Classe RepositoryEndereco.

---
+++ Livraria - Back-end/Livraria-API/Infraestructure/Data/Repositorys/RepositoryEndereco.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Entitys;
+
+namespace Infraestructure.Data.Repositorys
+{
+    public class RepositoryEndereco : RepositoryBase<Endereco>, IRepositoryEndereco
+    {
+        private readonly SqlContext _sqlContext;
+
+        public RepositoryEndereco(SqlContext sqlContext)
+            : base(sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+    }
+}
