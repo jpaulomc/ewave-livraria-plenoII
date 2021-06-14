5439473baf08861b0acfd5699e1391289a05a12b
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 18:27:25 -0400

    feat(Application): Correção do nome da interface IApplicationServiceGenero.

---
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/IApplicationServiceGenero.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos;
+using System.Collections.Generic;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationServiceGenero
+    {
+        void Add(GeneroDto generoDto);
+        void Update(GeneroDto generoDto);
+        IEnumerable<GeneroDto> GetAll();
+        GeneroDto GetById(int id);
+    }
+}
