be3b14d9691e86a46ae75c5ef0e1a3ad2e268cfa
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 17:35:54 -0400

    feat(Infraestructure): Implementação da Classe RepositoryUsuarioBloqueado.

---
+++ Livraria - Back-end/Livraria-API/Infraestructure/Data/Repositorys/RepositoryUsuarioBloqueado.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Interfaces.Repositorys;
+using Domain.Entitys;
+
+namespace Infraestructure.Data.Repositorys
+{
+    public class RepositoryUsuarioBloqueado : RepositoryBase<UsuarioBloqueado>, IRepositoryUsuarioBloqueado
+    {
+        private readonly SqlContext _sqlContext;
+
+        public RepositoryUsuarioBloqueado(SqlContext sqlContext)
+            : base(sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+    }
+}
