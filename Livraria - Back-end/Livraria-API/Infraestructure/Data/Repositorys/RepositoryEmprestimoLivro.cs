7483078d82b4c2ab1f6f909c54da9595d31dcebc
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 17:23:45 -0400

    feat(Infraestructure): Implementação da Classe RepositoryEmprestimoLivro.

--- Livraria - Back-end/Livraria-API/Infraestructure/Data/Repositorys/RepositoryEmprestimoLivro.cs
+++ Livraria - Back-end/Livraria-API/Infraestructure/Data/Repositorys/RepositoryEmprestimoLivro.cs
@@ -3,11 +3,11 @@ using Domain.Entitys;
 
 namespace Infraestructure.Data.Repositorys
 {
-    public class RepositoryAutor : RepositoryBase<Autor>, IRepositoryAutor
+    public class RepositoryEmprestimoLivro : RepositoryBase<EmprestimoLivro>, IRepositoryEmprestimoLivro
     {
         private readonly SqlContext _sqlContext;
 
-        public RepositoryAutor(SqlContext sqlContext)
+        public RepositoryEmprestimoLivro(SqlContext sqlContext)
             : base(sqlContext)
         {
             _sqlContext = sqlContext;
