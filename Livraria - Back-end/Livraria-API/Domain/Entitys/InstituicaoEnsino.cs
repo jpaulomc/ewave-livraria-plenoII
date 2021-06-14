59dcda13fa3f3d0f8a3bb74c472305a88e2fe5b2
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-14 10:15:37 -0400

    feat(Entity): Refatoração das classes Usuario e InstituicaoEnsino;

--- Livraria - Back-end/Livraria-API/Domain/Entitys/InstituicaoEnsino.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/InstituicaoEnsino.cs
@@ -2,10 +2,12 @@
 {
     public class InstituicaoEnsino : Base
     {
+        public int EnderecoID { get; set; }
         public string Nome { get; set; }
-        public Endereco Endereco { get; set; }
         public string CNPJ { get; set; }
         public string Telefone { get; set; }
         public bool Ativo { get; set; }
+
+        public Endereco Endereco { get; set; }
     }
 }
--- Livraria - Back-end/Livraria-API/Domain/Entitys/Usuario.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/Usuario.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain.Entitys
+﻿namespace Domain.Entitys
 {
     public class Usuario : Base
     {
+
+        public int EnderecoID { get; set; }
+        public int InstituicaoEnsinoID { get; set; }
         public string Nome { get; set; }
-        public Endereco Endereco { get; set; }
         public string CPF { get; set; }
-        public InstituicaoEnsino InstituicaoEnsino { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public bool Ativo { get; set; }
+
+        public Endereco Endereco { get; set; }
+        public InstituicaoEnsino InstituicaoEnsino { get; set; }
     }
 }
