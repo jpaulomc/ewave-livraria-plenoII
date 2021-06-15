01f1a5823579c5ed4d8935ac5ddeeb4e99b81416
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-15 11:54:35 -0400

    feat(Application): Implementação da classe ApplicationServiceUsuarioBloqueado.

--- Livraria - Back-end/Livraria-API/Application/Interfaces/Mappers/IMapperUsuarioBloqueado.cs
+++ Livraria - Back-end/Livraria-API/Application/Interfaces/Mappers/IMapperUsuarioBloqueado.cs
@@ -6,8 +6,8 @@ namespace Application.Interfaces.Mappers
 {
     public interface IMapperUsuarioBloqueado
     {
-        ReservaLivro MapperDtoToEntity(ReservaLivroDto reservaLivroDto);
-        IEnumerable<ReservaLivroDto> MapperListReservaLivrosDto(IEnumerable<ReservaLivro> reservaLivros);
-        ReservaLivroDto MapperEntityToDto(ReservaLivro reservaLivro);
+        UsuarioBloqueado MapperDtoToEntity(UsuarioBloqueadoDto usuarioBloqueadoDto);
+        IEnumerable<UsuarioBloqueadoDto> MapperListUsuarioBloqueadosDto(IEnumerable<UsuarioBloqueado> usuarioBloqueados);
+        UsuarioBloqueadoDto MapperEntityToDto(UsuarioBloqueado usuarioBloqueado);
     }
 }
