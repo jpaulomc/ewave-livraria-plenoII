1c738bf9278bdf7ca177c4cf795677a292c3ce6c
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-16 16:49:05 -0400

    feat(Application): Implementação da propriedade EnderecoID na classe InstituicaoEnsinoDto.

--- Livraria - Back-end/Livraria-API/Application/Dtos/InstituicaoEnsinoDto.cs
+++ Livraria - Back-end/Livraria-API/Application/Dtos/InstituicaoEnsinoDto.cs
@@ -3,8 +3,9 @@
     public class InstituicaoEnsinoDto
     {
         public int Id { get; set; }
+        public int EnderecoID { get; set; }
         public string Nome { get; set; }
-        public EnderecoDto Endereco { get; set; }
+        public EnderecoDto EnderecoDto { get; set; }
         public string CNPJ { get; set; }
         public string Telefone { get; set; }
         public bool Ativo { get; set; }
