66b771375557616c2561c56b152a74c758ae56ac
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-15 20:04:32 -0400

    feat(API): Implementação da classe AutorController;

---
+++ Livraria - Back-end/Livraria-API/API/Controllers/AutorController.cs
@@ -0,0 +1,69 @@
+﻿using Application.Dtos;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    public class AutorController : Controller
+    {
+        private readonly IApplicationServiceAutor _applicationServiceAutor;
+
+        public AutorController(IApplicationServiceAutor applicationServiceAutor)
+        {
+            _applicationServiceAutor = applicationServiceAutor;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicationServiceAutor.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_applicationServiceAutor.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] AutorDto autorDto)
+        {
+            try
+            {
+                if (autorDto == null)
+                {
+                    return NotFound();
+                }
+
+                _applicationServiceAutor.Add(autorDto);
+                return Ok("Autor Cadastrado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        [HttpPut]
+        public ActionResult Update([FromBody] AutorDto AutorDto)
+        {
+            try
+            {
+                if (AutorDto == null)
+                {
+                    return NotFound();
+                }
+
+                _applicationServiceAutor.Update(AutorDto);
+                return Ok("Autor Alterado com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
