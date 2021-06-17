5c83caa0b375320d48dea5fc87cdb265d0c90ab7
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-17 14:58:27 -0400

    feat(Domain.Entitys): Refatoração da classe Livro.

--- Livraria - Back-end/Livraria-API/Domain/Entitys/Livro.cs
+++ Livraria - Back-end/Livraria-API/Domain/Entitys/Livro.cs
@@ -10,8 +10,8 @@
     public class Livro : Base
     {
         public string Titulo { get; set; }
-        public string GeneroID { get; set; }
-        public string AutorID { get; set; }
+        public int GeneroID { get; set; }
+        public int AutorID { get; set; }
         public string Sinopse { get; set; }
         public string Capa { get; set; }
         public bool Ativo { get; set; }
