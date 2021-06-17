c4f794f0e2e4532cf6c5f8f71ba4413f3ca94f46
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-17 15:02:12 -0400

    feat(Application): Refatoração do método GetbyId da classe ApplicationServiceLivro.

--- Livraria - Back-end/Livraria-API/Application/ApplicationServiceLivro.cs
+++ Livraria - Back-end/Livraria-API/Application/ApplicationServiceLivro.cs
@@ -10,11 +10,16 @@ namespace Application
     {
         private readonly IServiceLivro _serviceLivro;
         private readonly IMapperLivro _mapperLivro;
+        private readonly IServiceGenero _serviceGenero;
+        private readonly IServiceAutor _serviceAutor;
 
-        public ApplicationServiceLivro(IServiceLivro serviceLivro, IMapperLivro mapperLivro)
+        public ApplicationServiceLivro(IServiceLivro serviceLivro, IMapperLivro mapperLivro,
+            IServiceGenero serviceGenero, IServiceAutor serviceAutor)
         {
             _serviceLivro = serviceLivro;
             _mapperLivro = mapperLivro;
+            _serviceGenero = serviceGenero;
+            _serviceAutor = serviceAutor;
         }
 
         public void Add(LivroDto livroDto)
@@ -32,6 +37,8 @@ namespace Application
         public LivroDto GetById(int id)
         {
             var livro = _serviceLivro.GetById(id);
+            livro.Genero = _serviceGenero.GetById(livro.GeneroID);
+            livro.Autor = _serviceAutor.GetById(livro.AutorID);
             return _mapperLivro.MapperEntityToDto(livro);
         }
 
