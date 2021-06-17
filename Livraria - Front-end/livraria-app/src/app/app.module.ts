abae2dd9a11b02d2b4238b9e79efbe4085c4db2f
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-17 17:58:41 -0400

    Criação do projeto Livraria - Front-end em Angular

---
+++ Livraria - Front-end/livraria-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LivroComponent } from './livro/livro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LivroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
