61405ad66cf572ba6bf21e6777c56b6ec56ef538
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-17 15:02:51 -0400

    feat(Application.Dtos): Refatoração da classe LivroDto.

--- Livraria - Back-end/Livraria-API/Application/Dtos/LivroDto.cs
+++ Livraria - Back-end/Livraria-API/Application/Dtos/LivroDto.cs
@@ -1,13 +1,19 @@
-﻿namespace Application.Dtos
+﻿using Domain.Entitys;
+
+namespace Application.Dtos
 {
     public class LivroDto
     {
         public int Id { get; set; }
+        public int GeneroID { get; set; }
+        public string? GeneroDescricao { get; set; }
+        public int AutorID { get; set; }
+        public string? AutorNome { get; set; }
         public string Titulo { get; set; }
         public string Sinopse { get; set; }
         public string Capa { get; set; }
         public bool Ativo { get; set; }
-        public GeneroDto GeneroDto { get; set; }
-        public AutorDto AutorDto { get; set; }
+        public StatusLivroEnum StatusLivro { get; set; }
+        
     }
 }
