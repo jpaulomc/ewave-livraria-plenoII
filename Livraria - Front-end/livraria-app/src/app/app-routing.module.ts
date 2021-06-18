d162acb89c8b35c86a460bbb01ca760b7c021c0d
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-18 10:31:48 -0400

    feat(Front-end.routes): Configuração inicial das rotas;

--- Livraria - Front-end/livraria-app/src/app/app-routing.module.ts
+++ Livraria - Front-end/livraria-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { LivroComponent } from './livro/livro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'livro', pathMatch: 'full'},
+  { path: 'livro', component: LivroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
