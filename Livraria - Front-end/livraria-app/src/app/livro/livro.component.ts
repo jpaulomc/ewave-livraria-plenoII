5155e6a0cbee0dfc8573ec7be9bdb9860050d2db
João Paulo de Moraes da Costa <j_paulomoraes@hotmail.com>
2021-06-18 10:31:09 -0400

    feat(Front-end.component): Implementação do método getAllLivros no LivroComponent.

--- Livraria - Front-end/livraria-app/src/app/livro/livro.component.ts
+++ Livraria - Front-end/livraria-app/src/app/livro/livro.component.ts
@@ -1,4 +1,6 @@
+import { LivroService } from './../livro.service';
 import { Component, OnInit } from '@angular/core';
+import { Livro } from '../models/livro.model';
 
 @Component({
   selector: 'app-livro',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroComponent implements OnInit {
 
-  constructor() { }
+  livros: any[] = [];
+
+  constructor(private service: LivroService) { }
 
   ngOnInit(): void {
+    this.service.getAllLivros().subscribe((livros: Livro[]) => {
+      console.table(livros);
+      this.livros = livros;
+    })
   }
 
 }
